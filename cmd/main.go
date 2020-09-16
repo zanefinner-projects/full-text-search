595156b86003a764b1e9ed1cee6b264f4aebc9f5
zanefinner <zanefinnergm@gmail.com>
2020-09-16 15:07:37 -0400

    clean up when done

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/davecgh/go-spew/spew"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -27,9 +28,43 @@ func main() {
 
 	//Create Sample records
 	createRecords(db)
+
+	//Clean Up
+	cleanUp(db)
 }
 func createRecords(db *gorm.DB) {
 	log.Println("Creating records...")
 	defer log.Println("Done!")
-
+	sampleBooks := [5]Book{}
+	sampleBooks[0] = Book{
+		Title:  "How to Code Well",
+		Author: "John Cena",
+		Year:   2004,
+	}
+	sampleBooks[1] = Book{
+		Title:  "Mind, Body, and Soul",
+		Author: "Yikes Smith",
+		Year:   2007,
+	}
+	sampleBooks[2] = Book{
+		Title:  "Yoga Basics",
+		Author: "Guru69",
+		Year:   2001,
+	}
+	sampleBooks[3] = Book{
+		Title:  "Mentoring 101",
+		Author: "Mr. Dr. Strange",
+		Year:   2020,
+	}
+	sampleBooks[4] = Book{
+		Title:  "Up to No Good",
+		Author: "Who Though",
+		Year:   2055,
+	}
+	log.Print("Records: ")
+	db.Create(&sampleBooks)
+	spew.Dump(sampleBooks)
+}
+func cleanUp(db *gorm.DB) {
+	db.Exec("DELETE FROM books")
 }
